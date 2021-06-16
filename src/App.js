1d5fbe60b0c8a33c8a1d0ad70cdc294abfc4cc28
Deltax2016 <deltaxussr@gmail.com>
2021-06-16 17:31:20 +0500

    Added Maker class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Web3 from 'web3';
+import { Maker } from './Utils.js';
 //import { Metamask } from './Utils';
 import React, {
     Component
@@ -12,18 +13,9 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import {
-    ChainId,
-    Token,
-    Fetcher,
-    Route,
-} from '@uniswap/sdk'
 
 import "./App.css";
-const tokenUSDT = '0xdac17f958d2ee523a2206206994597c13d831ec7';
-const tokenUSDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
-const tokenDAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-const tokenETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+
 
 function createData(name, balance, rate) {
   return { name, balance, rate };
@@ -52,66 +44,17 @@ class App extends Component {
 
             var web3 = new Web3(window.web3.currentProvider);
             await window.ethereum.enable();
-            var account = await web3.eth.getAccounts();
+            var maker = new Maker(web3);
+            await maker.init();
 
             //web3.eth.getAccounts().then();
-
-            let walletAddress = account[0];
-            const USDT = new Token(ChainId.MAINNET, tokenUSDT, 6)
-            const USDC = new Token(ChainId.MAINNET, tokenUSDC, 6)
-            const ETH = new Token(ChainId.MAINNET, tokenETH,18)
-            const DAI = new Token(ChainId.MAINNET, tokenDAI, 18)
-            
-            const duPair = await Fetcher.fetchPairData(DAI, USDT)
-            const uuPair = await Fetcher.fetchPairData(USDC, USDT)
-            const euPair = await Fetcher.fetchPairData(USDT, ETH)
-            const duRoute = new Route([duPair], DAI)
-            const uuRoute = new Route([uuPair], USDC)
-            const euRoute = new Route([euPair], ETH)
-            //console.log(duRoute);
-
-            let priceDAI = duRoute.midPrice.toSignificant(6);
-            let priceUSDC = uuRoute.midPrice.toSignificant(6);
-            let priceETH = euRoute.midPrice.toSignificant(6);
-            console.log(Math.round(priceDAI));
-            console.log(Math.round(priceUSDC));
-            console.log(priceETH);
-            //console.log(Math.round(duRoute.midPrice.toSignificant(6))); 
-            //console.log(Math.round(uuRoute.midPrice.toSignificant(6)));
-
-            let ABI = [
-                // balanceOf
-                {
-                    "constant": true,
-                    "inputs": [{
-                        "name": "_owner",
-                        "type": "address"
-                    }],
-                    "name": "balanceOf",
-                    "outputs": [{
-                        "name": "balance",
-                        "type": "uint256"
-                    }],
-                    "type": "function"
-                },
-            ];
-            // Get ERC20 Token contract instance
-            let contractDAI = new web3.eth.Contract(ABI, tokenDAI);
-            let contractUSDC = new web3.eth.Contract(ABI, tokenUSDC);
-            let contractETH = new web3.eth.Contract(ABI, tokenETH);
-            async function getBalance() {
-                let balanceDAI = await contractDAI.methods.balanceOf(walletAddress).call();
-                let balanceUSDC = await contractUSDC.methods.balanceOf(walletAddress).call();
-                let balanceETH= await contractETH.methods.balanceOf(walletAddress).call();
-                return [balanceDAI,balanceUSDC,balanceETH];
-            }
-            await getBalance().then(function(result) {
+            await maker.getBalance().then(function(result) {
                 console.log(result);
-                rows.push(createData('ETH',result[2],priceETH));
-                rows.push(createData('USDC',result[1],Math.round(priceUSDC)));
-                rows.push(createData('DAI',result[0],Math.round(priceDAI)));
-                
+                rows.push(createData('ETH',result[2],maker.priceETH));
+                rows.push(createData('USDC',result[1],Math.round(maker.priceUSDC)));
+                rows.push(createData('DAI',result[0],Math.round(maker.priceDAI)));
             });
+            
 
             console.log(rows)
             this.setState({isCompleted: 1});
--- src/Utils.js
+++ src/Utils.js
@@ -1,67 +1,72 @@
-import React, {
-    Component
-} from "react";
 
-class Metamask extends React.Component {
-   /*constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date()
-        };
-    }
-    componentDidMount = async() => {
-        try {
-            const chainId = ChainId.MAINNET
-            const decimals = 18
-            const web3 = new Web3(window.ethereum);
-            // get all accounts
-            const accounts = web3.eth.getAccounts();
-            let tokenAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7";
-            let tokenAddress2 = "0x6b175474e89094c44da98b954eedeac495271d0f";
-            let walletAddress = "0x73c38e1498102Cd42E402511455e6F95F8Dd1606";
-            //const DAI = new Token(chainId, tokenAddress, decimals);
-            const USDC = new Token(chainId, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6)
-            const DAI = new Token(chainId, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 6)
-            const DAIUSDCPair = await Fetcher.fetchPairData(DAI, USDC)
-            const route = new Route([DAIUSDCPair], USDC)
-            console.log(Math.round(route.midPrice.toSignificant(6))); // 202.081
+import {
+    ChainId,
+    Token,
+    Fetcher,
+    Route,
+} from '@uniswap/sdk'
 
-            let minABI = [
-                // balanceOf
-                {
-                    "constant": true,
-                    "inputs": [{
-                        "name": "_owner",
-                        "type": "address"
-                    }],
-                    "name": "balanceOf",
-                    "outputs": [{
-                        "name": "balance",
-                        "type": "uint256"
-                    }],
-                    "type": "function"
-                },
-            ];
-            // Get ERC20 Token contract instance
-            let contract = new web3.eth.Contract(minABI, tokenAddress);
-            async function getBalance() {
-                let balance = await contract.methods.balanceOf(walletAddress).call();
-                return balance;
-            }
-            getBalance().then(function(result) {
-                console.log(result);
-            });
-        } catch (error) {
-            // Catch any errors for any of the above operations.
-            alert(
-                `Failed to load web3, accounts, or contract. Check console for details.`,
-            );
-            console.error(error);
-        }
-    };*/
-    render() {
-        return <h1 > Привет, {
-            this.props.name
-        } < /h1>;
+const tokenUSDT = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+const tokenUSDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+const tokenDAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+const tokenETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+
+export class Maker {
+    constructor(web) {
+        this.web3 = web;
     }
+    async init() {
+        this.account = await this.web3.eth.getAccounts();
+        this.walletAddress = this.account[0];
+        this.USDT = new Token(ChainId.MAINNET, tokenUSDT, 6)
+        this.USDC = new Token(ChainId.MAINNET, tokenUSDC, 6)
+        this.ETH = new Token(ChainId.MAINNET, tokenETH,18)
+        this.DAI = new Token(ChainId.MAINNET, tokenDAI, 18)
+
+        this.duPair = await Fetcher.fetchPairData(this.DAI, this.USDT)
+        this.uuPair = await Fetcher.fetchPairData(this.USDC, this.USDT)
+        this.euPair = await Fetcher.fetchPairData(this.USDT, this.ETH)
+        this.duRoute = new Route([this.duPair], this.DAI)
+        this.uuRoute = new Route([this.uuPair], this.USDC)
+        this.euRoute = new Route([this.euPair], this.ETH)
+                //console.log(duRoute);
+
+        this.priceDAI = this.duRoute.midPrice.toSignificant(6);
+        this.priceUSDC = this.uuRoute.midPrice.toSignificant(6);
+        this.priceETH = this.euRoute.midPrice.toSignificant(6);
+        console.log(Math.round(this.priceDAI));
+        console.log(Math.round(this.priceUSDC));
+        console.log(this.priceETH);
+        //console.log(Math.round(duRoute.midPrice.toSignificant(6))); 
+        //console.log(Math.round(uuRoute.midPrice.toSignificant(6)));
+
+        this.ABI = [
+            // balanceOf
+            {
+                "constant": true,
+                "inputs": [{
+                    "name": "_owner",
+                    "type": "address"
+                }],
+                "name": "balanceOf",
+                "outputs": [{
+                    "name": "balance",
+                    "type": "uint256"
+                }],
+                "type": "function"
+            },
+        ];
+        // Get ERC20 Token contract instance
+        this.contractDAI = new this.web3.eth.Contract(this.ABI, tokenDAI);
+        this.contractUSDC = new this.web3.eth.Contract(this.ABI, tokenUSDC);
+        this.contractETH = new this.web3.eth.Contract(this.ABI, tokenETH);
+      }
+    
+    async getBalance() {
+        this.balanceDAI = await this.contractDAI.methods.balanceOf(this.walletAddress).call();
+        this.balanceUSDC = await this.contractUSDC.methods.balanceOf(this.walletAddress).call();
+        this.balanceETH= await this.contractETH.methods.balanceOf(this.walletAddress).call();
+        return [this.balanceDAI,this.balanceUSDC,this.balanceETH];
+    }
+            
 }
